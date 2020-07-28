e1c7fa86d77273ab286990a83b0d451ea332da32
Bywebb306 <bywebb306@gmail.com>
2020-07-28 15:24:09 -0400

    updating localhost

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false",
+  process.env.MONGODB_URI || "mongodb://localhost:27017",
   {
     useCreateIndex: true,
     useNewUrlParser: true
